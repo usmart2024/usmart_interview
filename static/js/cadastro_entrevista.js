2bc9513b4677b62663df7574fd11bfe7c9cfdca8
ubuntu22 <rodolfo.dsantos@grupomateus.com>
2024-06-09 12:15:13 -0300

    Abrir popup ao detalhar entrevista

--- static/js/cadastro_entrevista.js
+++ static/js/cadastro_entrevista.js
@@ -66,29 +66,63 @@ $(document).ready(function() {
         }
     });
 
+   $(document).on("click", ".view-details", function() {
+      var $row = $(this).closest("tr");
+      var topic = $row.find("td:nth-child(1)").text();
+      var subtopic = $row.find("td:nth-child(2)").text();
+      var question = $row.find("td:nth-child(3)").text();
+      var answer = $row.find("td:nth-child(4)").text();
 
+      var data = [
+          { topic: "Java", subtopic: "OOP", question: "What is polymorphism in object-oriented programming?", answer: "Polymorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming..." },
+          { topic: "JavaScript", subtopic: "ES6", question: "What is a promise in JavaScript and how do you use it?", answer: "A promise is an object that represents the eventual completion (or failure) of an asynchronous operation, and its resulting value." },
+          { topic: "Python", subtopic: "Data Types", question: "What are Python dictionaries and how do you use them?", answer: "Dictionaries are a type of collection in Python that are unordered, changeable, and indexed. They are written with curly brackets, and they have keys and values." },
+          { topic: "Java", subtopic: "OOP", question: "What is polymorphism in object-oriented programming?", answer: "Polymorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming..." },
+          { topic: "JavaScript", subtopic: "ES6", question: "What is a promise in JavaScript and how do you use it?", answer: "A promise is an object that represents the eventual completion (or failure) of an asynchronous operation, and its resulting value." },
+          { topic: "Python", subtopic: "Data Types", question: "What are Python dictionaries and how do you use them?", answer: "Dictionaries are a type of collection in Python that are unordered, changeable, and indexed. They are written with curly brackets, and they have keys and values." },
+          { topic: "Java", subtopic: "OOP", question: "What is polymorphism in object-oriented programming?", answer: "Polymorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming..." },
+          { topic: "JavaScript", subtopic: "ES6", question: "What is a promise in JavaScript and how do you use it?", answer: "A promise is an object that represents the eventual completion (or failure) of an asynchronous operation, and its resulting value." },
+          { topic: "Python", subtopic: "Data Types", question: "What are Python dictionaries and how do you use them?", answer: "Dictionaries are a type of collection in Python that are unordered, changeable, and indexed. They are written with curly brackets, and they have keys and values." },
+          { topic: "Java", subtopic: "OOP", question: "What is polymorphism in object-oriented programming?", answer: "Polymorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming..." },
+          { topic: "JavaScript", subtopic: "ES6", question: "What is a promise in JavaScript and how do you use it?", answer: "A promise is an object that represents the eventual completion (or failure) of an asynchronous operation, and its resulting value." },
+          { topic: "Python", subtopic: "Data Types", question: "What are Python dictionaries and how do you use them?", answer: "Dictionaries are a type of collection in Python that are unordered, changeable, and indexed. They are written with curly brackets, and they have keys and values." },
+          { topic: "Java", subtopic: "OOP", question: "What is polymorphism in object-oriented programming?", answer: "Polymorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming..." }
+      ];
 
-    // Evento para abrir o modal com os detalhes
-    $(document).on("click", ".view-details", function() {
-        var $row = $(this).closest("tr");
-        var topic = $row.find("td:nth-child(1)").text();
-        var subtopic = $row.find("td:nth-child(2)").text();
-        var question = $row.find("td:nth-child(3)").text();
-        var answer = $row.find("td:nth-child(4)").text();
-
-        var modalContent = "<p><strong>Topic:</strong> " + topic + "</p>";
-        modalContent += "<p><strong>Subtopic:</strong> " + subtopic + "</p>";
-        modalContent += "<p><strong>Question:</strong> " + question + "</p>";
-        modalContent += "<p><strong>Answer:</strong> " + answer + "</p>";
-
-        $("#detailModal .modal-body").html(modalContent);
+    var modalContent = '';
+    data.forEach(function(item) {
+        modalContent += "<div style='border: 1px solid #ddd; padding: 10px; margin-bottom: 10px;'>";
+        modalContent += "<p><strong>Stack:</strong> " + item.topic + "</p>";
+        modalContent += "<p><strong>Description:</strong> " + item.subtopic + "</p>";
+        modalContent += "<p><strong>Score:</strong> " + item.question + "</p>";
+        modalContent += "<p><strong>Answer:</strong> " + item.answer + "</p>";
+        modalContent += "</div>";
     });
 
-    displayTable(currentPage);
+    $("#detailModal .modal-body").html(modalContent);
+    $("#detailModal").modal("show");
 });
 
-  $(document).ready(function() {
+    displayTable(currentPage);
+
+    // Redirecionar ao clicar no botão Start
     $('#startButton').on('click', function() {
-      window.location.href = 'http://localhost:5000/';
+        window.location.href = 'http://localhost:5000/';
     });
-  });
+
+    $('#detailModal').on('hidden.bs.modal', function () {
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+    });
+
+    $('.close, #closeButton').on('click', function() {
+        $('#detailModal').modal('hide');
+    });
+
+    $('#cancelButton').on('click', function() {
+        $('.modal-backdrop').remove();
+         $('#interviewModal').modal('hide');
+
+    });
+
+});
